3f485202ac16a81dbea2f61cdd3f2b1848c8f21c
Haruray <syafiqfaray2@gmail.com>
2022-10-12 07:44:38 +0700

    bugfixes about player_modifier not changed correctly

--- AlphaBetaPruneBot.py
+++ AlphaBetaPruneBot.py
@@ -10,9 +10,10 @@ class AlphaBetaPruneBot(Bot):
     def obj_function(self, state:GameState):
         #get player modifier
         result = 0
+        player_modifier = -1 if state.player1_turn else 1
         for row in state.board_status:
             for square in row:
-                if (abs(square) ==  4):
+                if (square ==  4 * player_modifier):
                     #Fungsi nilai(kotak) akan memberikan nilai 1 untuk kotak player tersebut
                     result+=1
                 elif (abs(square) ==  3):
@@ -21,7 +22,7 @@ class AlphaBetaPruneBot(Bot):
                 elif (abs(square) ==  1 or square==0):
                     #0.5 untuk kotak dengan 0 atau 1 sisi
                     result+=0.5
-        return result
+        return result * player_modifier
 
     def check_if_board_full(self,state:GameState):
         all_row_marked = np.all(state.row_status == 1)
@@ -30,18 +31,22 @@ class AlphaBetaPruneBot(Bot):
     
     def get_step_minimal(self,bs1:BotStep, bs2:BotStep):
         if (bs1 < bs2):
-            bs1.set_steps(copy.deepcopy(bs1.get_steps()))
-            return bs1
+            res = BotStep(bs1.get_value())
+            res.set_steps(copy.deepcopy(bs1.get_steps()))
+            return res
         else:
-            bs2.set_steps(copy.deepcopy(bs2.get_steps()))
-            return bs2
+            res = BotStep(bs2.get_value())
+            res.set_steps(copy.deepcopy(bs2.get_steps()))
+            return res
     def get_step_maximal(self,bs1:BotStep, bs2:BotStep):
         if (bs1 > bs2):
-            bs1.set_steps(copy.deepcopy(bs1.get_steps()))
-            return bs1
+            res = BotStep(bs1.get_value())
+            res.set_steps(copy.deepcopy(bs1.get_steps()))
+            return res
         else:
-            bs2.set_steps(copy.deepcopy(bs2.get_steps()))
-            return bs2
+            res = BotStep(bs2.get_value())
+            res.set_steps(copy.deepcopy(bs2.get_steps()))
+            return res
 
     def minimax(self,curr_state:GameState, alpha:BotStep, beta:BotStep,player_modifier, steps_recorded:list, time_count, depth=0):
         #function ini bakal rekursif, jadi basisnya adalah board_statusnya udah penuh atau waktu udah 5 detik
@@ -59,6 +64,7 @@ class AlphaBetaPruneBot(Bot):
             return val
 
         #minimizing
+        end_loop = False
         if (player_modifier == -1):
 
             min_val = BotStep(float('inf')) # negative infinity
@@ -73,7 +79,7 @@ class AlphaBetaPruneBot(Bot):
                     copy.deepcopy(curr_state.board_status),
                     copy.deepcopy(curr_state.row_status),
                     copy.deepcopy(curr_state.col_status),
-                    curr_state.player1_turn
+                    False if player_modifier==-1 else True
                 )
 
                 if i < (number_of_dots-1) and j < (number_of_dots-1):
@@ -93,39 +99,41 @@ class AlphaBetaPruneBot(Bot):
 
                 steps_recorded.clear()
 
-                if beta >= alpha:
+                if beta <= alpha:
+                    end_loop = True
                     break
 
             #loop dari data garis col yang belum diisi
-            rows,cols = np.where(curr_state.col_status == 0)
-            for i, j in zip(rows, cols):
-                #generate semua kemungkinan langkah yang mungkin terjadi
-                #buat new state, habis itu copy 
-                new_state = GameState(
-                    copy.deepcopy(curr_state.board_status),
-                    copy.deepcopy(curr_state.row_status),
-                    copy.deepcopy(curr_state.col_status),
-                    curr_state.player1_turn
-                )
+            if not end_loop:
+                rows,cols = np.where(curr_state.col_status == 0)
+                for i, j in zip(rows, cols):
+                    #generate semua kemungkinan langkah yang mungkin terjadi
+                    #buat new state, habis itu copy 
+                    new_state = GameState(
+                        copy.deepcopy(curr_state.board_status),
+                        copy.deepcopy(curr_state.row_status),
+                        copy.deepcopy(curr_state.col_status),
+                        False if player_modifier==-1 else True
+                    )
 
-                if i < (number_of_dots-1) and j < (number_of_dots-1):
-                    new_state.board_status[i][j] = (abs(new_state.board_status[i][j]) + 1) * player_modifier
-                if (j > 0):
-                    new_state.board_status[i][j-1] = (abs(new_state.board_status[i][j-1]) + 1) * player_modifier
-                new_state.col_status[i][j] = 1
+                    if i < (number_of_dots-1) and j < (number_of_dots-1):
+                        new_state.board_status[i][j] = (abs(new_state.board_status[i][j]) + 1) * player_modifier
+                    if (j > 0):
+                        new_state.board_status[i][j-1] = (abs(new_state.board_status[i][j-1]) + 1) * player_modifier
+                    new_state.col_status[i][j] = 1
 
-                #state telah dibuat, saatnya rekursif
-                steps_recorded.append(["col",i,j])
-                val = self.minimax(new_state, alpha, beta ,1, steps_recorded,time_count,depth+1)
+                    #state telah dibuat, saatnya rekursif
+                    steps_recorded.append(["col",i,j])
+                    val = self.minimax(new_state, alpha, beta ,1, steps_recorded,time_count,depth+1)
 
-                #bandingkan min_val dengan val yang didapat. Ambil paling kecil
-                min_val = self.get_step_minimal(min_val, val)
-                beta = self.get_step_minimal(beta, min_val)
+                    #bandingkan min_val dengan val yang didapat. Ambil paling kecil
+                    min_val = self.get_step_minimal(min_val, val)
+                    beta = self.get_step_minimal(beta, min_val)
 
-                steps_recorded.clear()
+                    steps_recorded.clear()
 
-                if beta >= alpha:
-                    break  
+                    if beta <= alpha:
+                        break  
             return min_val 
 
         #maximizing
@@ -142,7 +150,7 @@ class AlphaBetaPruneBot(Bot):
                     copy.deepcopy(curr_state.board_status),
                     copy.deepcopy(curr_state.row_status),
                     copy.deepcopy(curr_state.col_status),
-                    curr_state.player1_turn
+                    False if player_modifier==-1 else True
                 )
 
                 if j < (number_of_dots-1) and i < (number_of_dots-1):
@@ -162,37 +170,39 @@ class AlphaBetaPruneBot(Bot):
                 steps_recorded.clear()
 
                 if beta <= alpha:
+                    end_loop = True
                     break   
             
             #loop dari data garis col yang belum diisi
-            rows,cols = np.where(curr_state.col_status == 0)
-            for i, j in zip(rows, cols):
-                #generate semua kemungkinan langkah yang mungkin terjadi
-                #buat new state, habis itu copy 
-                new_state = GameState(
-                    copy.deepcopy(curr_state.board_status),
-                    copy.deepcopy(curr_state.row_status),
-                    copy.deepcopy(curr_state.col_status),
-                    curr_state.player1_turn
-                )
+            if not end_loop:
+                rows,cols = np.where(curr_state.col_status == 0)
+                for i, j in zip(rows, cols):
+                    #generate semua kemungkinan langkah yang mungkin terjadi
+                    #buat new state, habis itu copy 
+                    new_state = GameState(
+                        copy.deepcopy(curr_state.board_status),
+                        copy.deepcopy(curr_state.row_status),
+                        copy.deepcopy(curr_state.col_status),
+                        False if player_modifier==-1 else True
+                    )
 
-                if i < (number_of_dots-1) and j < (number_of_dots-1):
-                    new_state.board_status[i][j] = (abs(new_state.board_status[i][j]) + 1) * player_modifier
-                if (j > 0):
-                    new_state.board_status[i][j-1] = (abs(new_state.board_status[i][j-1]) + 1) * player_modifier
-                new_state.col_status[i][j] = 1
+                    if i < (number_of_dots-1) and j < (number_of_dots-1):
+                        new_state.board_status[i][j] = (abs(new_state.board_status[i][j]) + 1) * player_modifier
+                    if (j > 0):
+                        new_state.board_status[i][j-1] = (abs(new_state.board_status[i][j-1]) + 1) * player_modifier
+                    new_state.col_status[i][j] = 1
 
-                #state telah dibuat, saatnya rekursif
-                steps_recorded.append(["col",i,j])
-                val = self.minimax(new_state, alpha, beta ,-1, steps_recorded, time_count,depth+1)
-                #bandingkan min_val dengan val yang didapat. Ambil paling kecil
-                max_val = self.get_step_maximal(max_val, val)
-                alpha = self.get_step_maximal(alpha, max_val)
+                    #state telah dibuat, saatnya rekursif
+                    steps_recorded.append(["col",i,j])
+                    val = self.minimax(new_state, alpha, beta ,-1, steps_recorded, time_count,depth+1)
+                    #bandingkan min_val dengan val yang didapat. Ambil paling kecil
+                    max_val = self.get_step_maximal(max_val, val)
+                    alpha = self.get_step_maximal(alpha, max_val)
 
-                steps_recorded.clear()
+                    steps_recorded.clear()
 
-                if beta <= alpha:
-                    break
+                    if beta <= alpha:
+                        break
             return max_val    
         
     
@@ -201,8 +211,6 @@ class AlphaBetaPruneBot(Bot):
         beta = BotStep(float("inf"))
         result = self.minimax(state, alpha, beta, 1, [], time.time())
 
-        print(result.get_value())
-        print(result.get_steps())
         step_type = result.get_first_step_type()
         step_pos = (result.get_first_step_y(), result.get_first_step_x())
         print(step_type, step_pos)
